c9a086d7a163822a0f46f777e56b9155939c8101
HungNH <nguyen.huy.hungb@framgia.com>
2016-10-15 18:41:59 +0700

    updating-users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20161016004348) do
-
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
@@ -23,6 +22,7 @@ ActiveRecord::Schema.define(version: 20161016004348) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
