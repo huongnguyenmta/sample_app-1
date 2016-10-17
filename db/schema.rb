5215c5e0671e47aae93f3ecd9484427a956ae7bb
HungNH <nguyen.huy.hungb@framgia.com>
2016-10-17 09:25:34 +0700

    Password reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161016004348) do
+
+ActiveRecord::Schema.define(version: 20161016032719) do
+
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
@@ -22,6 +24,8 @@ ActiveRecord::Schema.define(version: 20161016004348) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
 
     t.index ["email"], name: "index_users_on_email", unique: true
   end
