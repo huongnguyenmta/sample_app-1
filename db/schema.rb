f14b6acc1d47df29ad116c9da3c94578a8791330
HungNH <nguyen.huy.hungb@framgia.com>
2016-10-17 09:25:34 +0700

    Password reset

--- db/schema.rb
+++ db/schema.rb
@@ -35,11 +35,7 @@ ActiveRecord::Schema.define(version: 20161017082345) do
     t.datetime "activated_at"
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
-    t.string   "password_digest"
-    t.string   "remember_digest"
-    t.boolean  "admin",           default: false
+
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
