6e6ba7b1129efa6191e3ee37472a0478b39c5fe7
HungNH <nguyen.huy.hungb@framgia.com>
2016-10-18 11:55:42 +0700

    following users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get "password_resets/new"
-
-  get "password_resets/edit"
-
   root "static_pages#home"
+  get "password_resets/new"
+  get "password_resets/edit"
   get "/help", to: "static_pages#help"
   get "/about", to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
@@ -12,8 +10,12 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
-  resources :microposts, only: [:create, :destroy]
+  resources :microposts, :relationships, only: [:create, :destroy]
 end
