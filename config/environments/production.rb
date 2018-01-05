49cc8dfefe9889806791ccd4816b545988ed306f
Nguyen Binh Dieu <nguyen.binh.dieu@framgia.com>
2018-01-05 19:55:53 +0700

    Add mail server production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,15 +90,18 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Gmail configuration
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'dieunb.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'example.com',
-    user_name:            ENV['EMAIL_USER'],
-    password:             ENV['EMAIL_PASS'],
-    authentication:       'plain',
-    enable_starttls_auto: true
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "dieunb.herokuapp.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["EMAIL_USER"],
+    password: ENV["EMAIL_PASS"]
   }
 end
